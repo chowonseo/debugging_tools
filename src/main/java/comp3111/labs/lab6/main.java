c30cd1e98b50edf1acf98b5493ff1dc4db8b41c9
chowonseo <wsnumber1@naver.com>
2022-10-27 18:49:47 +0800

    edited main.java

--- src/main/java/comp3111/labs/lab6/main.java
+++ src/main/java/comp3111/labs/lab6/main.java
@@ -6,8 +6,30 @@ package comp3111.labs.lab6;
 public class main {
 	public static void main(String[] args) {
 		Animal animals[] = new Animal[10];
-		for (Animal a : animals)
-			a = new Animal();	
+//		int test = 0;
+//		for (Animal a : animals) {
+////			test++;
+//			a = new Animal();
+//			test++;
+//		}
+		for (int i=0;i<10;i++) {
+			animals[i] = new Animal();
+		}
+		/*
+		 * The error is introduced at the point where animals[iii].isAlive() is called.
+		 * This raises an error, as animals[iii] is a null.
+		 * The previous code:
+		 * for(Animal a: animals) a = new Animal;
+		 * only creates Animal object on the local variable a inside the for loop
+		 * and deleted after each iteration in the for loop.
+		 * (Does not store the newly created Animal object to the animals[]
+		 * resulting in animals[] with 10 null values.
+		 * Therefore, I have instead modified the code to directly point to the
+		 * blocks in animals[i] and instantiated Animal object. Hence, 10 animal
+		 * objects are stored in animals[] which we are safe to call the member
+		 * operations of animal objects
+		 */
+		
 		for (int iii = 0; iii < 10; iii++) {
 			int ii = 0;
 			for (; ii < 100 && animals[iii].isAlive() ; ii++) {
